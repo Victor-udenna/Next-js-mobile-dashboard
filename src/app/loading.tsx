9e8dd7f3e23585a36d5fac23a069253c4e2db29a
Victor udenna <victorudenna@gmail.com>
2023-12-03 08:57:24 +0100

    Edited route files

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,20 @@
+"use client"
+
+import { Bars } from "react-loader-spinner"
+
 export default function Loading(){
     return (
-        <h1>Pls wait</h1>
+       <div className="w-screen h-screen flex flex-col items-center justify-center">
+
+ <Bars
+  height="80"
+  width="80"
+  color="#1a51eb"
+  ariaLabel="bars-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+/>
+       </div>
     )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,6 @@
 export default function Notfound(){
-
 return(
-    <h1>Not Found</h1>
+<h1>Not Found</h1>
 )
 
 }
